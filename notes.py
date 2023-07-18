aba2d148278bd47f6a8ea51da43fefeb112136c1
Eduardo Guimarães <edu.guisantos@outlook.com>
2023-07-18 03:27:47 -0300

    Source code translated to English, changed buttons size and check icon

--- notes.py
+++ notes.py
@@ -1,4 +1,4 @@
-# Por Eduardo G.
+# By Eduardo G.
 
 import PySimpleGUI as sg
 from datetime import date
@@ -7,32 +7,32 @@ import os
 
 sg.theme('SystemDefault1')
 
-lista = []          # Todas as tarefas a serem realizadas
-checkeds = []       # Tarefas realizadas
-uncheckeds = []     # Tarefas não realizadas
+tasks = []          # All the tasks to be completed
+checkeds = []       # Completed tasks
+uncheckeds = []     # Uncompleted tasks
 
 current_directory = os.getcwd()
 
-if 'Histórico' and 'Tarefas' in os.listdir(current_directory):
+if 'Historic' and 'Tasks' in os.listdir(current_directory):
     pass
 else:
-    os.mkdir('Histórico')
-    os.mkdir('Tarefas')
-    os.mknod('{}/Tarefas/tarefas'.format(current_directory))
-    os.mknod('{}/Tarefas/checkeds'.format(current_directory))
-    os.mknod('{}/Tarefas/uncheckeds'.format(current_directory))
+    os.mkdir('Historic')
+    os.mkdir('Tasks')
+    os.mknod('{}/Tasks/tasks'.format(current_directory))
+    os.mknod('{}/Tasks/checkeds'.format(current_directory))
+    os.mknod('{}/Tasks/uncheckeds'.format(current_directory))
 
-history_exists = os.path.exists('{}/Histórico/{}'.format(current_directory, date.today()))
+history_exists = os.path.exists('{}/Historic/{}'.format(current_directory, date.today()))
 
-with open(r'{}/Tarefas/tarefas'.format(current_directory), 'r+') as fp:
+with open(r'{}/Tasks/tasks'.format(current_directory), 'r+') as fp:
     if history_exists == False:
         fp.truncate(0)
 
     for line in fp:
         x = line[:-1]
-        lista.append(x)
+        tasks.append(x)
         
-with open(r'{}/Tarefas/checkeds'.format(current_directory), 'r+') as fp:
+with open(r'{}/Tasks/checkeds'.format(current_directory), 'r+') as fp:
     if history_exists == False:
         fp.truncate(0)
 
@@ -40,7 +40,7 @@ with open(r'{}/Tarefas/checkeds'.format(current_directory), 'r+') as fp:
         x = line[:-1]
         checkeds.append(x)
 
-with open(r'{}/Tarefas/uncheckeds'.format(current_directory), 'r+') as fp:
+with open(r'{}/Tasks/uncheckeds'.format(current_directory), 'r+') as fp:
     if history_exists == False:
         fp.truncate(0)
 
@@ -49,11 +49,11 @@ with open(r'{}/Tarefas/uncheckeds'.format(current_directory), 'r+') as fp:
         uncheckeds.append(x)
 
 treedata = sg.TreeData()
-history_path = ('{}/Histórico/'.format(current_directory))
+history_path = ('{}/Historic/'.format(current_directory))
 history = os.listdir(history_path)
 
 for i in history:
-    file = open('{}/Histórico/{}'.format(current_directory, i))
+    file = open('{}/Historic/{}'.format(current_directory, i))
     lines = file.readlines()
     treedata.Insert("", i, i, lines[0][11:])
     for line in lines:
@@ -75,13 +75,12 @@ class MyWindow(sg.Window):
         win_width, win_height = self.size
         x, y = (screen_width - win_width)//2, (screen_height - win_height)//2
         self.move(x, y)
-
    
-# Função para adicionar checkbox às tarefas.
-    # item - Nome da tarefa
-    # index - Localização da tarefa
-    # checked - Se a checkbox da arefa esará marcada ou não
-    # is_deleted - Se a tarefa será deletada
+# Function to add checkbox to the tasks.
+    # item - Task's name
+    # index - Task's index
+    # checked - If the task's checkbox will be checked or not
+    # is_deleted - If the task will be deleted
 
 def checkbox(item, index, checked, is_deleted):
     icon_uncheck = '[ ]'
@@ -90,47 +89,48 @@ def checkbox(item, index, checked, is_deleted):
     if checked == True and is_deleted == False:
         if item not in checkeds and item not in uncheckeds:
             checkeds.append(item)
-            lista.insert(index, '{} {}'.format(icon_check, item))
+            tasks.insert(index, '{} {}'.format(icon_check, item))
+            window['-confirm-'].update('☑')
 
         elif item in checkeds and item not in uncheckeds:
             checkeds.remove(item)
             uncheckeds.append(item)
-            lista.insert(index, '{} {}'.format(icon_uncheck, item))
+            tasks.insert(index, '{} {}'.format(icon_uncheck, item))
+            window['-confirm-'].update('☑')
 
         elif item not in checkeds and item in uncheckeds:
             uncheckeds.remove(item)
             checkeds.append(item)
-            lista.insert(index, '{} {}'.format(icon_check, item))
-
+            tasks.insert(index, '{} {}'.format(icon_check, item))
+            window['-confirm-'].update('☐')
 
     if checked == False and is_deleted == False:
             if item not in uncheckeds and item not in checkeds:
                 uncheckeds.append(item)
-                lista.insert(index, '{} {}'.format(icon_uncheck, item))
+                tasks.insert(index, '{} {}'.format(icon_uncheck, item))
 
     if is_deleted == True and item[4:] in uncheckeds:
-        lista.remove(item)
+        tasks.remove(item)
         uncheckeds.remove(item[4:])
 
     if is_deleted == True and item[4:] in checkeds:
-        lista.remove(item)
+        tasks.remove(item)
         checkeds.remove(item[4:])
 
-
 def _percent():
-    if len(checkeds) == 0 or len(lista) == 0:
+    if len(checkeds) == 0 or len(tasks) == 0:
         return 0
     else:
-        percent = (len(checkeds) / len(lista)) * 100
+        percent = (len(checkeds) / len(tasks)) * 100
         return float(f'{percent:.2f}')
 
 def _save():
-    tarefas_file = '{}/Tarefas/tarefas'.format(current_directory)    
-    checkeds_file = '{}/Tarefas/checkeds'.format(current_directory)    
-    uncheckeds_file = '{}/Tarefas/uncheckeds'.format(current_directory)    
+    tasks_file = '{}/Tasks/tasks'.format(current_directory)    
+    checkeds_file = '{}/Tasks/checkeds'.format(current_directory)    
+    uncheckeds_file = '{}/Tasks/uncheckeds'.format(current_directory)    
 
-    with open(r'{}'.format(tarefas_file), 'w') as fp:
-        for i in lista:
+    with open(r'{}'.format(tasks_file), 'w') as fp:
+        for i in tasks:
             fp.write("{}\n".format(i))
 
     with open(r'{}'.format(checkeds_file), 'w') as fp:
@@ -141,29 +141,28 @@ def _save():
         for i in uncheckeds:
             fp.write("{}\n".format(i))
 
-    with open(r'{}/Histórico/{}'.format(current_directory, date.today()), 'w') as fp:
+    with open(r'{}/Historic/{}'.format(current_directory, date.today()), 'w') as fp:
         fp.write("Concluído: {}%".format(_percent()))
         if _percent() == 100.0:
             fp.write(" - PARABÉNS!!\n\n")
         else:
             fp.write("\n\n")
-        for i in lista:
+        for i in tasks:
             fp.write("{}\n".format(i))
-
 
 layout_l = [ [sg.InputText(do_not_clear = False, font='Iosevka 14', s=(45), expand_x=True),
             sg.Push(),
             sg.Button('Adicionar', bind_return_key=True)],
-           [sg.Listbox(values=lista, s=(10,15), expand_x=True, expand_y=True, font='Iosevka 16', key = '-tarefas-')],
+           [sg.Listbox(values=tasks, s=(10,15), expand_x=True, expand_y=True, font='Iosevka 16', enable_events=True, key = '-tasks-')],
            [sg.T("Concluído: {}{}".format(_percent(), '%'), key="-percent-")],
            [sg.Button('Histórico'),
             sg.Push(),
             sg.Button('Sair', key="-SAIR1-")] ]
 
-layout_r = [ [sg.Button('Con')],
-             [sg.Button('▲')],
-             [sg.Button('▼')],
-             [sg.Button('🗑')]]
+layout_r = [ [sg.Button('☑', font='_ 16', key='-confirm-')],
+             [sg.Button('▲', font='_ 14')],
+             [sg.Button('▼', font='_ 14')],
+             [sg.Button('🗑', font='_ 16')]]
 
 layout_historic = [ [sg.Tree(treedata, ['Concluído'], expand_x=True, expand_y=True, font='Iosevka 12', col0_width=(53), key='-TREE-')],
                     [sg.Button("<"),
@@ -175,8 +174,7 @@ layout = [ [sg.T('Tarefas', font='_ 14', justification='c', expand_x=True, s=(52
             sg.Col(layout_r, p=0, key='-COL1_R-'),
             sg.Col(layout_historic, key='-COL2-', visible=False, expand_x=True, expand_y=True)] ]
 
-window = MyWindow('Lista de Tarefas', layout, font='Iosveka 10', finalize=True, location=(0,0))
-#window = sg.window('Lista de Tarefas', layout, font='Iosveka 10', finalize=True, location=(0,0))
+window = MyWindow('Lista de Tasks', layout, font='Iosveka 10', finalize=True, location=(0,0))
 window.my_move_to_center()
 
 while True:
@@ -191,17 +189,16 @@ while True:
             pass
         else:
             checkbox(values[0], 9999999, False, False)
-            window['-tarefas-'].update(values=lista, set_to_index=[lista.index("[ ] {}".format(values[0]))], scroll_to_index=(lista.index("[ ] {}".format(values[0]))))   
+            window['-tasks-'].update(values=tasks, set_to_index=[tasks.index("[ ] {}".format(values[0]))], scroll_to_index=(tasks.index("[ ] {}".format(values[0]))))   
             _percent()
             window['-percent-'].update('Concluído: {}{}'.format(_percent(), '%'))
             _save()
             window['-TREE-'].update(treedata)
                 
     elif event == '🗑':
-        tarefa = window['-tarefas-'].get_indexes()
-        #lista.pop(tarefa[0])
-        checkbox(values['-tarefas-'][0], tarefa[0], False, True)
-        window['-tarefas-'].update(values=lista, set_to_index=[tarefa[0]], scroll_to_index=(tarefa[0]))
+        tarefa = window['-tasks-'].get_indexes()
+        checkbox(values['-tasks-'][0], tarefa[0], False, True)
+        window['-tasks-'].update(values=tasks, set_to_index=[tarefa[0]], scroll_to_index=(tarefa[0]))
         _percent()
         window['-percent-'].update('Concluído: {}{}'.format(_percent(), '%'))
         _save()
@@ -209,23 +206,22 @@ while True:
 
     elif event == '▲':
         temp = {}
-        tarefa = window['-tarefas-'].get_indexes()
-        temp["item"] = values['-tarefas-'][0][4:]
+        tarefa = window['-tasks-'].get_indexes()
+        temp["item"] = values['-tasks-'][0][4:]
         temp["index"] = tarefa[0]
-       # checkbox(temp["item"], (temp["index"] - 1), False, False)
         if temp["item"] in checkeds:
             if temp["index"] > 0:
-                lista.pop(tarefa[0])
-                lista.insert((temp["index"] - 1), "[X] {}".format(temp["item"]))
-                window['-tarefas-'].update(values=lista, set_to_index=[temp["index"] - 1], scroll_to_index=(temp["index"] - 1))
+                tasks.pop(tarefa[0])
+                tasks.insert((temp["index"] - 1), "[X] {}".format(temp["item"]))
+                window['-tasks-'].update(values=tasks, set_to_index=[temp["index"] - 1], scroll_to_index=(temp["index"] - 1))
                 window['-TREE-'].update(treedata)
             else:
                 pass
         else:
             if temp["index"] > 0:
-                lista.pop(tarefa[0])
-                lista.insert((temp["index"] - 1), "[ ] {}".format(temp["item"]))
-                window['-tarefas-'].update(values=lista, set_to_index=[temp["index"] - 1], scroll_to_index=(temp["index"] - 1))
+                tasks.pop(tarefa[0])
+                tasks.insert((temp["index"] - 1), "[ ] {}".format(temp["item"]))
+                window['-tasks-'].update(values=tasks, set_to_index=[temp["index"] - 1], scroll_to_index=(temp["index"] - 1))
                 window['-TREE-'].update(treedata)
             else:
                 pass
@@ -233,36 +229,35 @@ while True:
 
     elif event == '▼':
         temp = {}
-        tarefa = window['-tarefas-'].get_indexes()
-        temp["item"] = values['-tarefas-'][0][4:]
+        tarefa = window['-tasks-'].get_indexes()
+        temp["item"] = values['-tasks-'][0][4:]
         temp["index"] = tarefa[0]
-       # checkbox(temp["item"], (temp["index"] - 1), False, False)
         if temp["item"] in checkeds:
-            if temp["index"] < (len(lista) - 1):
-                lista.pop(tarefa[0])
-                lista.insert((temp["index"] + 1), "[X] {}".format(temp["item"]))
-                window['-tarefas-'].update(values=lista, set_to_index=[temp["index"] + 1], scroll_to_index=(temp["index"] + 1))
+            if temp["index"] < (len(tasks) - 1):
+                tasks.pop(tarefa[0])
+                tasks.insert((temp["index"] + 1), "[X] {}".format(temp["item"]))
+                window['-tasks-'].update(values=tasks, set_to_index=[temp["index"] + 1], scroll_to_index=(temp["index"] + 1))
                 window['-TREE-'].update(treedata)
             else:
                 pass
         else:
-            if temp["index"] < (len(lista) - 1):
-                lista.pop(tarefa[0])
-                lista.insert((temp["index"] + 1), "[ ] {}".format(temp["item"]))
-                window['-tarefas-'].update(values=lista, set_to_index=[temp["index"] + 1], scroll_to_index=(temp["index"] + 1))
+            if temp["index"] < (len(tasks) - 1):
+                tasks.pop(tarefa[0])
+                tasks.insert((temp["index"] + 1), "[ ] {}".format(temp["item"]))
+                window['-tasks-'].update(values=tasks, set_to_index=[temp["index"] + 1], scroll_to_index=(temp["index"] + 1))
                 window['-TREE-'].update(treedata)
             else:
                 pass
         _save()
         
-    elif event == 'Con':
+    elif event == '-confirm-':
         temp = {}
-        tarefa = window['-tarefas-'].get_indexes()
-        temp["item"] = values['-tarefas-'][0][4:]
+        tarefa = window['-tasks-'].get_indexes()
+        temp["item"] = values['-tasks-'][0][4:]
         temp["index"] = tarefa[0]
-        lista.pop(tarefa[0])
+        tasks.pop(tarefa[0])
         checkbox(temp["item"], temp["index"], True, False)
-        window['-tarefas-'].update(values=lista, set_to_index=[temp["index"]], scroll_to_index=(temp["index"]))
+        window['-tasks-'].update(values=tasks, set_to_index=[temp["index"]], scroll_to_index=(temp["index"]))
         _percent()
         window['-percent-'].update('Concluído: {}{}'.format(_percent(), '%'))
         _save()
@@ -278,4 +273,12 @@ while True:
         window['-COL1_L-'].update(visible=True)
         window['-COL1_R-'].update(visible=True)
 
+    elif event == '-tasks-':
+        if values['-tasks-'][0][4:] in checkeds:
+            window['-confirm-'].update('☐')
+        else:
+            print(values['-tasks-'][0])
+            print(checkeds)
+            window['-confirm-'].update('☑')
+            
 window.close()
